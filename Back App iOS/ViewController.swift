22a5dfdcdb3c75bddf846a247c891a3b7b4b7038
MoSchaub <moritz@pfaender.net>
2020-06-26 21:58:30 +0200

    added RecipeDetailViewController

--- Back App iOS/ViewController.swift
+++ Back App iOS/ViewController.swift
@@ -7,15 +7,18 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UITableViewController {
     var recipeStore = RecipeStore()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         title = "Back App"
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        navigationItem.rightBarButtonItem = editButtonItem
+        navigationItem.leftBarButtonItem = editButtonItem
     }
     
     // MARK: - Data Source
@@ -31,9 +34,8 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         if section == 0 {
             return "Rezepte"
-        } else {
-            return "other"
         }
+        return nil
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -41,11 +43,12 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0, recipeStore.recipes.count > indexPath.row{
+        if indexPath.section == 0, recipeStore.recipes.count > indexPath.row{ //recipe section
             let cell = tableView.dequeueReusableCell(withIdentifier: "recipe", for: indexPath)
             let recipe = recipeStore.recipes[indexPath.row]
             cell.textLabel?.text = recipe.name
             cell.textLabel!.font = UIFont.preferredFont(forTextStyle: .headline)
+            cell.accessoryType = .disclosureIndicator
             if let imageData = recipe.imageString {
                 cell.imageView?.image = UIImage(data: imageData)
             } else {
@@ -54,7 +57,19 @@ class ViewController: UITableViewController {
             return cell
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "other", for: indexPath)
-            cell.textLabel?.text = "\(indexPath.row)"
+            switch indexPath.row {
+            case 0:
+                cell.textLabel?.text = "Raumthemperatur: \(recipeStore.roomThemperature)ºC"
+            case 1:
+                cell.textLabel?.text = "Rezepte aus Datei importieren"
+            case 2:
+                cell.textLabel?.text = "alle Rezepte exportieren"
+            case 3:
+                cell.textLabel?.text = "Über diese App"
+            default:
+                cell.textLabel?.text = "\(indexPath.row)"
+            }
+            cell.accessoryType = .disclosureIndicator
             return cell
         }
     }
@@ -87,7 +102,33 @@ class ViewController: UITableViewController {
         } else {return false}
     }
     
+    // MARK: - Delegate
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0, recipeStore.recipes.count > indexPath.row {
+            // 1: try loading the "Detail" view controller and typecasting it to be RecipeDetailViewController
+            if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? RecipeDetailViewController {
+                // 2: success! Set its recipe property
+                vc.recipe = recipeStore.recipes[indexPath.row]
+
+                // 3: now push it onto the navigation controller
+                navigationController?.pushViewController(vc, animated: true)
+            }
+        } else if indexPath.section == 1 {
+            let row = indexPath.row
+            if row == 0 {
+                print("temp")
+                // room temperaturePicker
+            } else if row == 1 {
+                print("import")
+            } else if row == 2 {
+                print("export")
+            } else if row == 3 {
+                print("about")
+            }
+            
+        }
+    }
 
 
 }
