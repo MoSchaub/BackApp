6433cd30cbdb056ec0ec71c7631eb26c83617c83
Moritz Schaub <moritz@pfaender.net>
2020-10-11 15:57:05 +0200

    removed seperators and added Section headers

--- Back App iOS/Controllers/Compact/StepDetail/Components/IngredientDetailViewController.swift
+++ Back App iOS/Controllers/Compact/StepDetail/Components/IngredientDetailViewController.swift
@@ -11,6 +11,9 @@ import BakingRecipeFoundation
 import BakingRecipeCells
 import BakingRecipeStrings
 import BakingRecipeItems
+import BakingRecipeSections
+
+typealias DataSource = UITableViewDiffableDataSource<IngredientDetailSection, TextItem>
 
 class IngredientDetailViewController: UITableViewController {
     
@@ -67,6 +70,7 @@ extension IngredientDetailViewController {
         super.loadView()
         registerCells()
         setupNavigationBar()
+        tableView.separatorStyle = .none
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -114,14 +118,9 @@ private extension IngredientDetailViewController {
     
 }
 
-enum IngredientDetailSection: CaseIterable {
-    case name, amount, type
-}
 
 // MARK: - DataSource and Snapshot
 private extension IngredientDetailViewController {
-    
-    typealias DataSource = UITableViewDiffableDataSource<IngredientDetailSection, TextItem>
     
     ///create the diffableDataSource
     func makeDiffableDataSource() -> DataSource {
@@ -132,7 +131,7 @@ private extension IngredientDetailViewController {
             ingredient.amount = Double(amountText.trimmingCharacters(in: .letters).trimmingCharacters(in: .whitespacesAndNewlines)) ?? 0
             return ingredient.formatted(rest: amountText)
         }
-        return DataSource(tableView: tableView) { (tableView, indexPath, textItem) -> UITableViewCell? in
+        return IngredientDetailDataSource(tableView: tableView) { (tableView, indexPath, textItem) -> UITableViewCell? in
             if let textFieldItem = textItem as? TextFieldItem, let cell = tableView.dequeueReusableCell(withIdentifier: Strings.nameCell, for: indexPath) as? TextFieldCell {
                 cell.textField.text = textFieldItem.text
                 cell.textField.placeholder = Strings.name
@@ -201,7 +200,6 @@ private extension IngredientDetailViewController {
 }
 
 
-
 // MARK: - Type Selection
 extension IngredientDetailViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -265,149 +263,16 @@ extension IngredientDetailViewController {
     }
 }
 
-//import UIKit
-//import BakingRecipeFoundation
-//import BakingRecipeStrings
-//import BakingRecipeCells
-//
-//class IngredientDetailViewController: UITableViewController {
-//
-//    // MARK: - Properties
-//    private var ingredient: Ingredient {
-//        didSet {
-//            DispatchQueue.main.async {
-//                self.setupNavigationBar()
-//            }
-//            update(oldValue: oldValue)
-//        }
-//    }
-//
-//    var creating: Bool
-//    var saveIngredient: ((Ingredient) -> Void)
-//
-//    private func update(oldValue: Ingredient) {
-//        DispatchQueue.global(qos: .background).async {
-//            if !self.creating, oldValue != self.ingredient {
-//                self.saveIngredient(self.ingredient)
-//            }
-//        }
-//    }
-//
-//    init(ingredient: Ingredient, creating: Bool, saveIngredient: @escaping (Ingredient) -> ()) {
-//        self.ingredient = ingredient
-//        self.saveIngredient = saveIngredient
-//        self.creating = creating
-//        super.init(style: .insetGrouped)
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError(Strings.init_coder_not_implemented)
-//    }
-//
-//    // MARK: - Start functions
-//
-//    override func loadView() {
-//        super.loadView()
-//        self.tableView = UITableView(frame: tableView.frame, style: .insetGrouped)
-//        registerCells()
-//    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        setupNavigationBar()
-//    }
-//
-//    // MARK: - NavigationBar
-//
-//    private func setupNavigationBar() {
-//        if creating {
-//            navigationItem.rightBarButtonItem = .init(barButtonSystemItem: .save, target: self, action: #selector(addIngredient))
-//        }
-//        title = self.ingredient.formattedName
-//        navigationController?.navigationBar.prefersLargeTitles = true
-//    }
-//
-//    @objc private func addIngredient(_ sender: UIBarButtonItem) {
-//        saveIngredient(ingredient)
-//        navigationController?.popViewController(animated: true)
-//    }
-//
-//    // MARK: - Rows and Sections
-//
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        return 3
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 1
-//    }
-//
-//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        if section == 0 {
-//            return Strings.name
-//        } else if section == 1 {
-//            return Strings.amount
-//        } else {
-//            return nil
-//        }
-//    }
-//
-//    // MARK: - Cells
-//
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        if indexPath.section == 0 {
-//            return makeNameCell()
-//        } else if indexPath.section == 1 {
-//            return makeAmountCell()
-//        } else {
-//            return makeTypeCell()
-//        }
-//    }
-//
-//    private func registerCells() {
-//        tableView.register(TextFieldCell.self, forCellReuseIdentifier: Strings.nameCell)
-//        tableView.register(AmountCell.self, forCellReuseIdentifier: Strings.amountCell)
-//        tableView.register(DetailCell.self, forCellReuseIdentifier: Strings.bulkLiquidCell)
-//    }
-//
-//    private func makeNameCell() -> TextFieldCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: Strings.nameCell) as! TextFieldCell
-//        cell.textField.text = ingredient.name
-//        cell.textField.placeholder = Strings.name
-//        cell.selectionStyle = .none
-//        cell.textChanged = { text in
-//            self.ingredient.name = text
-//        }
-//        cell.backgroundColor = UIColor.backgroundColor
-//        return cell
-//    }
-//
-//    private func makeAmountCell() -> AmountCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: Strings.amountCell) as! AmountCell
-//        cell.setUp(with: ingredient, format: format)
-//        return cell
-//    }
-//
-//    private func format(amountText: String) -> String {
-//        guard Double(amountText.trimmingCharacters(in: .letters).trimmingCharacters(in: .whitespacesAndNewlines)) != nil else { return "" }
-//        ingredient.amount = Double(amountText.trimmingCharacters(in: .letters).trimmingCharacters(in: .whitespacesAndNewlines)) ?? 0
-//        return ingredient.formatted(rest: amountText)
-//    }
-//
-//    private func makeTypeCell() -> DetailCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: Strings.bulkLiquidCell) as! DetailCell
-//
-//        cell.textLabel?.text = Strings.type
-//        cell.detailTextLabel?.text = ingredient.type.name
-//        cell.accessoryType = .none
-//
-//        return cell
-//    }
-//
-//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        if indexPath.section == 2 { // make sure its in the type section
-//            // - TODO: navigate to List for Picking type
-//        }
-//    }
-//
-//}
+
+// MARK: - Section headers
+class IngredientDetailDataSource: DataSource {
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0: return Strings.name
+        case 1: return Strings.amount
+        default:
+            return ""
+        }
+    }
+}
