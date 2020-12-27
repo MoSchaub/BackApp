db7dba97938c1f546cd38b3d328e0cf1bcbaeca2
Moritz Schaub <moritz@pfaender.net>
2020-12-27 13:47:42 +0100

    added BackAppJson Package

---
+++ BackAppJson/README.md
@@ -0,0 +1,3 @@
+# BackAppJson
+
+A description of this package.
---
+++ BackAppJson/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import BackAppJsonTests
+
+var tests = [XCTestCaseEntry]()
+tests += BackAppJsonTests.allTests()
+XCTMain(tests)
