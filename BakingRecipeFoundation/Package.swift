8d9900dfe89844b45811a2b3d4b3b69aab296e8c
Moritz Schaub <moritz@pfaender.net>
2021-09-23 23:27:55 +0200

    fixed libs for xcode 13

--- BakingRecipeFoundation/Package.swift
+++ BakingRecipeFoundation/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "BakingRecipeFoundation",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -16,7 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "BakingRecipeStrings", path: "/Users/moritzschaub/Developer/Swift/ios/BrotApp2/BakingRecipeStrings"),
-        .package(name: "GRDB", url: "https://github.com/groue/GRDB.swift", from: "5.8.0")
+        .package(name: "GRDB", path: "/Users/moritzschaub/Developer/Swift/Database/GRDB.swift")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
