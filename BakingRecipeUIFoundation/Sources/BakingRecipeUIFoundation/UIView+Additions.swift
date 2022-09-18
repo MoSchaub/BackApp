bbe7a6b00a9bddb2b278ca84ab1f4cbda8e71c5e
MoSchaub <moritz@pfaender.net>
2022-09-18 04:01:51 +0200

    fixed some warnings

--- BakingRecipeUIFoundation/Sources/BakingRecipeUIFoundation/UIView+Additions.swift
+++ BakingRecipeUIFoundation/Sources/BakingRecipeUIFoundation/UIView+Additions.swift
@@ -27,7 +27,7 @@ public struct AnchoredConstraints {
 extension UIView {
     
     @discardableResult
-    open func anchor(_ anchors: Anchor...) -> AnchoredConstraints {
+    func anchor(_ anchors: Anchor...) -> AnchoredConstraints {
         translatesAutoresizingMaskIntoConstraints = false
         var anchoredConstraints = AnchoredConstraints()
         anchors.forEach { anchor in
@@ -62,7 +62,7 @@ extension UIView {
     }
     
     @discardableResult
-    open func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) -> AnchoredConstraints {
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) -> AnchoredConstraints {
         
         translatesAutoresizingMaskIntoConstraints = false
         var anchoredConstraints = AnchoredConstraints()
@@ -97,32 +97,32 @@ extension UIView {
     }
     
     @discardableResult
-    open func fillSuperview(padding: UIEdgeInsets = .zero) -> AnchoredConstraints {
+    public func fillSuperview(padding: UIEdgeInsets = .zero) -> AnchoredConstraints {
         translatesAutoresizingMaskIntoConstraints = false
         let anchoredConstraints = AnchoredConstraints()
         guard let superviewTopAnchor = superview?.topAnchor,
-            let superviewBottomAnchor = superview?.bottomAnchor,
-            let superviewLeadingAnchor = superview?.leadingAnchor,
-            let superviewTrailingAnchor = superview?.trailingAnchor else {
-                return anchoredConstraints
+              let superviewBottomAnchor = superview?.bottomAnchor,
+              let superviewLeadingAnchor = superview?.leadingAnchor,
+              let superviewTrailingAnchor = superview?.trailingAnchor else {
+            return anchoredConstraints
         }
         
         return anchor(top: superviewTopAnchor, leading: superviewLeadingAnchor, bottom: superviewBottomAnchor, trailing: superviewTrailingAnchor, padding: padding)
     }
     
     @discardableResult
-    open func fillSuperviewSafeAreaLayoutGuide(padding: UIEdgeInsets = .zero) -> AnchoredConstraints {
+    func fillSuperviewSafeAreaLayoutGuide(padding: UIEdgeInsets = .zero) -> AnchoredConstraints {
         let anchoredConstraints = AnchoredConstraints()
         guard let superviewTopAnchor = superview?.safeAreaLayoutGuide.topAnchor,
-            let superviewBottomAnchor = superview?.safeAreaLayoutGuide.bottomAnchor,
-            let superviewLeadingAnchor = superview?.safeAreaLayoutGuide.leadingAnchor,
-            let superviewTrailingAnchor = superview?.safeAreaLayoutGuide.trailingAnchor else {
-                return anchoredConstraints
+              let superviewBottomAnchor = superview?.safeAreaLayoutGuide.bottomAnchor,
+              let superviewLeadingAnchor = superview?.safeAreaLayoutGuide.leadingAnchor,
+              let superviewTrailingAnchor = superview?.safeAreaLayoutGuide.trailingAnchor else {
+            return anchoredConstraints
         }
         return anchor(top: superviewTopAnchor, leading: superviewLeadingAnchor, bottom: superviewBottomAnchor, trailing: superviewTrailingAnchor, padding: padding)
     }
     
-    open func centerInSuperview(size: CGSize = .zero) {
+    func centerInSuperview(size: CGSize = .zero) {
         translatesAutoresizingMaskIntoConstraints = false
         if let superviewCenterXAnchor = superview?.centerXAnchor {
             centerXAnchor.constraint(equalTo: superviewCenterXAnchor).isActive = true
@@ -141,24 +141,24 @@ extension UIView {
         }
     }
     
-    open func centerXTo(_ anchor: NSLayoutXAxisAnchor) {
+    func centerXTo(_ anchor: NSLayoutXAxisAnchor) {
         translatesAutoresizingMaskIntoConstraints = false
         centerXAnchor.constraint(equalTo: anchor).isActive = true
     }
     
-    open func centerYTo(_ anchor: NSLayoutYAxisAnchor) {
+    func centerYTo(_ anchor: NSLayoutYAxisAnchor) {
         translatesAutoresizingMaskIntoConstraints = false
         centerYAnchor.constraint(equalTo: anchor).isActive = true
     }
     
-    open func centerXToSuperview() {
+    func centerXToSuperview() {
         translatesAutoresizingMaskIntoConstraints = false
         if let superviewCenterXAnchor = superview?.centerXAnchor {
             centerXAnchor.constraint(equalTo: superviewCenterXAnchor).isActive = true
         }
     }
     
-    open func centerYToSuperview() {
+    func centerYToSuperview() {
         translatesAutoresizingMaskIntoConstraints = false
         if let superviewCenterYAnchor = superview?.centerYAnchor {
             centerYAnchor.constraint(equalTo: superviewCenterYAnchor).isActive = true
@@ -166,7 +166,7 @@ extension UIView {
     }
     
     @discardableResult
-    open func constrainHeight(_ constant: CGFloat) -> AnchoredConstraints {
+    func constrainHeight(_ constant: CGFloat) -> AnchoredConstraints {
         translatesAutoresizingMaskIntoConstraints = false
         var anchoredConstraints = AnchoredConstraints()
         anchoredConstraints.height = heightAnchor.constraint(equalToConstant: constant)
@@ -175,7 +175,7 @@ extension UIView {
     }
     
     @discardableResult
-    open func constrainWidth(_ constant: CGFloat) -> AnchoredConstraints {
+    func constrainWidth(_ constant: CGFloat) -> AnchoredConstraints {
         translatesAutoresizingMaskIntoConstraints = false
         var anchoredConstraints = AnchoredConstraints()
         anchoredConstraints.width = widthAnchor.constraint(equalToConstant: constant)
@@ -183,7 +183,7 @@ extension UIView {
         return anchoredConstraints
     }
     
-    open func setupShadow(opacity: Float = 0, radius: CGFloat = 0, offset: CGSize = .zero, color: UIColor = .black) {
+    func setupShadow(opacity: Float = 0, radius: CGFloat = 0, offset: CGSize = .zero, color: UIColor = .black) {
         layer.shadowOpacity = opacity
         layer.shadowRadius = radius
         layer.shadowOffset = offset
