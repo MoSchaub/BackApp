9878e9a2cfcbbd1931728019cccf325c56c21e6d
Moritz Schaub <moritz-github@hadiko.de>
2024-12-28 22:20:39 +0100

    fix license string

--- Back App iOS/Cells/AmountCell.swift
+++ Back App iOS/Cells/AmountCell.swift
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2024 Moritz Schaub <moritz@pfaender.net>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
-
 //
 //  AmountCell.swift
 //  
--- Back App iOS/Controllers/Compact/RecipeDetail/Tests/EditRecipeViewTest.swift
+++ Back App iOS/Controllers/Compact/RecipeDetail/Tests/EditRecipeViewTest.swift
@@ -1,4 +1,3 @@
-// Copyright © 2024 Moritz Schaub. All rights reserved.
 // SPDX-FileCopyrightText: 2024 Moritz Schaub <moritz@pfaender.net>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
