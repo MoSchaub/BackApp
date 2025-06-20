d52d2cdb7b24b0af2fb58ec214c318c250ca98ca
Moritz Schaub <moritz-github@hadiko.de>
2025-06-20 16:21:47 +0200

    update licensing header for UITextView+ Files

--- Back App iOS/Cells/UITextView+Placeholder.h
+++ Back App iOS/Cells/UITextView+Placeholder.h
@@ -1,7 +1,6 @@
-// The MIT License (MIT)
+// SPDX-FileCopyrightText: 2014 Suyeol Jeon <http:xoul.kr>
 //
-// Copyright (c) 2014 Suyeol Jeon (http:xoul.kr)
-//
+// SPDX-License-Identifier: MIT
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
