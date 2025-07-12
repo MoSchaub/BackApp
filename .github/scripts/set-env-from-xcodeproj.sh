58e2fd734b8734dc8118922487461924f8e029b7
Moritz Schaub <moritz-github@hadiko.de>
2025-07-12 23:34:54 +0200

    remove unused scripts

--- .github/scripts/set-env-from-xcodeproj.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-DEBUG_SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
-RELEASE_SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[1]')"
-
-PRODUCT_NAME="$(xcodebuild -scheme "$DEBUG_SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
-echo "::set-env name=PRODUCT_NAME::$PRODUCT_NAME"
